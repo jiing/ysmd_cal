85e0f77e467a65f2bc9ce8891cee40d34d78c399
Jiing Deng <jiing@jiing.org>
2012-03-15 22:10:29 +0800

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,52 @@
+class WelcomeController < ApplicationController
+  def index
+
+     if (!params[:p].blank?)
+       p = params[:p].to_f
+       logger.debug p
+     end
+
+     if (!params[:w].blank?)
+       w = params[:w].to_f
+       logger.debug w
+     end
+
+     if(!params[:h].blank?)
+       h = params[:h].to_f
+       logger.debug h
+     end
+
+     if(!params[:i].blank?)
+       i = params[:i].to_f
+       logger.debug i
+     end
+
+     if(!params[:sigma_max].blank?)
+       sigma_max = params[:sigma_max].to_f
+       logger.debug sigma_max
+     end
+
+     e= 7*10**5
+
+     @ans = 0
+     if( p && w && h && i && e)
+        sigma_max = BigDecimal.new("0");
+        sigma_max = (p*w*h**4) / (1920.0*e*i) *(5.0-(w/h)**2)**2
+        @ans = sigma_max
+        logger.debug sigma_max
+     elsif( sigma_max && w && h && i && e)
+          p =  BigDecimal.new("0");
+         p = sigma_max * (1920.0*e*i) *(5.0-(w/h)**2)**2 /w/(h**4)
+         @ans = p
+          logger.debug p
+      elsif( sigma_max && w && h && p && e)
+           i =  BigDecimal.new("0");
+           i = p / sigma_max *w *(h**4)/((5.0-(w/h)**2)**2)/1920.0/e
+           @ans = i
+            logger.debug i
+      end
+
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YsmdCal::Application.config.secret_token = 'b0a2e5a5b47db621b8225424f1202ebf8c6bdfbcd37e4290b4ba393888b0565063ea1871b2de8e6c4c002c25fc190da97c993ad511569784a5da4355492b838e'
